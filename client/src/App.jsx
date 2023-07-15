d397cf384e4726fdab829acb55c7e81013db7204
QuinnP2910 <72775766+QuinnP2910@users.noreply.github.com>
2023-07-14 17:20:52 -0700

    Split into SidebarNote

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import AlbumSidebar from "./components/AlbumSidebar.jsx";
 import NoteModal from "./components/NoteModal.jsx";
 import EditNoteModal from "./components/EditNoteModal.jsx";
+import SidebarNote from "./components/SidebarNote.jsx";
 
 function App() {
     const [playbackProgress, setPlaybackProgress] = useState(-1);
@@ -31,7 +32,6 @@ function App() {
                     return response.json();
                 })
                 .then((data) => {
-                    console.log(Math.abs(data.progress_ms - sliderProgress));
                     if (Math.abs(data.progress_ms - sliderProgress) < 2000) {
                         setScrubbing(false);
                     }
@@ -140,7 +140,6 @@ function App() {
                 let formID = "#note-form" + i;
                 let textareaID = "#noteInput" + i;
                 $(textareaID).on("keydown", function (event) {
-                    console.log("keydown");
                     if (event.key === "Enter") {
                         $(buttonID).click();
                     }
@@ -148,7 +147,6 @@ function App() {
                 $(buttonID)
                     .off("click")
                     .click(async function () {
-                        console.log("click");
                         let noteData = $(formID).serializeArray();
                         let editedNote = new Note(
                             noteData[0].value,
@@ -209,49 +207,17 @@ function App() {
                             releaseDate={releaseDate}
                         />
                         {notes.map((note, i) => (
-                            <>
-                                <h2
-                                    className="timestamp d-inline-block"
-                                    onClick={() =>
-                                        setUserPlaybackProgress(
-                                            timestampToMilliseconds(
-                                                note.timestamp
-                                            )
-                                        )
-                                    }
-                                >
-                                    {note.timestamp}
-                                </h2>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="16"
-                                    height="16"
-                                    fill="#d8e9a8"
-                                    className="bi bi-pencil-square"
-                                    viewBox="0 0 16 16"
-                                    data-bs-toggle="modal"
-                                    data-bs-target={"#editNoteInterface" + i}
-                                >
-                                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                                    <path
-                                        fillRule="evenodd"
-                                        d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
-                                    />
-                                </svg>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="16"
-                                    height="16"
-                                    fill="#d8e9a8"
-                                    className="bi bi-trash"
-                                    viewBox="0 0 16 16"
-                                    onClick={() => notes.splice(i, 1)}
-                                >
-                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
-                                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
-                                </svg>
-                                <h3>{note.note}</h3>
-                            </>
+                            <SidebarNote
+                                note={note}
+                                notes={notes}
+                                key={i}
+                                index={i}
+                                onClick={() =>
+                                    setUserPlaybackProgress(
+                                        timestampToMilliseconds(note.timestamp)
+                                    )
+                                }
+                            />
                         ))}
                     </div>
                     <div className="col-9">
@@ -373,6 +339,7 @@ function App() {
             <NoteModal tempTimeStamp={tempTimeStamp} />
             {notes.map((note, i) => (
                 <EditNoteModal
+                    key={i}
                     index={i}
                     timestamp={note.timestamp}
                     length={note.length}
