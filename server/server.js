bc2534dc0b585dbde444258e87ad502e5d1b5419
QuinnP2910 <72775766+QuinnP2910@users.noreply.github.com>
2023-08-02 20:49:38 -0700

    Add support for environment port

--- server/server.js
+++ server/server.js
@@ -441,6 +441,7 @@ app.post("/api", async (req, res) => {
     }
 });
 
-app.listen(5000, () => {
-    console.log("Server started on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log("Server started on port " + PORT);
 });
