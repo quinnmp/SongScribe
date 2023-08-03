b7d883c4102a19883167b9f672a78e828301f376
QuinnP2910 <72775766+QuinnP2910@users.noreply.github.com>
2023-08-03 11:24:58 -0700

    Add "beta" to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,7 +28,7 @@ function Header() {
                             </svg>
                         </div>
                         <div className="col-5 ps-0">
-                            <h1>SpotiScribe</h1>
+                            <h1>SpotiScribe [beta]</h1>
                         </div>
                     </div>
                 </div>
