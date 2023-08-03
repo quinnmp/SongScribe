e9d5a661647ec95dc0a926ed1906b4ec66f01caa
QuinnP2910 <72775766+QuinnP2910@users.noreply.github.com>
2023-08-02 22:53:20 -0700

    Add just the production url to cors protection

--- server/server.js
+++ server/server.js
@@ -7,8 +7,10 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://127.0.0.1:5173" }));
 require("dotenv").config();
+
+const corsUrl = "https://spotiscribe.onrender.com";
+app.use(cors({ origin: corsUrl }));
 
 mongoose.connect(
     `mongodb+srv://miles:${process.env.DB_PASSWORD}@userscribes.rzgicer.mongodb.net/`
